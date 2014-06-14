862480097609a296bc49e8e0570938323763255a
Alexandre <abrantes.alexandre.richard@gmail.com>
2014-06-14 13:23:01 +0200

    doc

--- catalog.h
+++ catalog.h
@@ -4,6 +4,13 @@
 #include "uvmanager.h"
 #include "utilities.h"
 
+/**
+ * @brief The Catalog class
+ * L'onglet catalogue affiche l'ensemble des uvs, comme proposé sur l'ENT, rubrique 'consultation des enseignement'.
+ * Il permet de trier selon les catégories d'uvs (CS,TSH...)  et leur ouverture (printemps, automne).
+ * Il est possible de visualiser les uvs propres à un cursus.
+ */
+
 class Catalog: public QWidget
 {
     Q_OBJECT
@@ -12,9 +19,24 @@ public:
     Catalog();
 
 public slots:
+    /**
+     * @brief criteriaChanged
+     * Slot appelé lorsque l'utilisateur change de critère de tri.
+     * Active le bouton 'rechercher'.
+     */
     void criteriaChanged();
-    void editCatalog(bool edit);
+
+    /**
+     * @brief selectDegree
+     * @param title
+     * Sélection d'un cursus particulier. Affiche la liste des sous-cursus associés.
+     */
     void selectDegree(const QString &title);
+
+    /**
+     * @brief updateCatalog
+     * Rafraîchit la page selon les nouveaux critères.
+     */
     void updateCatalog();
 
 private:
--- connection.h
+++ connection.h
@@ -3,6 +3,12 @@
 #include <QtWidgets>
 #include "uvmanager.h"
 
+/**
+ * @brief The Connection class
+ * L'onglet connexion permet à l'étudiant de s'identifier ou de s'ajouter à la base.
+ * Après l'identification, les autres onglets sont mis à jour et complétés avec les informations propres à l'utilisateur.
+ */
+
 class Connection: public QWidget
 {
     Q_OBJECT
@@ -11,9 +17,19 @@ public:
     Connection();
 
 public slots:
+    /**
+     * @brief login
+     * Appelle la fonction de connexion de l'uvmanager.
+     * Vérifie la validité du login (8 caractères).
+     * Affiche un message.
+     */
     void login();
 
 signals:
+    /**
+     * @brief connected
+     * Signal émis lorsque la connexion réussie.
+     */
     void connected();
 
 private:
